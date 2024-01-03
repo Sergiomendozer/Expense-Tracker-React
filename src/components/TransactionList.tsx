9c1a670d28db4928e50c647a8b860894a8e456ba
Sergio Mendoza <sergiomendozansx@gmail.com>
2024-01-03 20:41:25 +0000

    Update 3 files ts to tsx

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,26 +1,28 @@
 import React, { useContext } from "react";
-import { Transaction } from "../components/Transaction"; 
-
-import { GlobalContext } from "../context/GlobalState"; 
+import { Transaction } from "../components/Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext); // Accessing transactions from the global context
 
-  {/* Mapping through each transaction and rendering a Transaction component for each */}
-
+  {
+    /* Mapping through each transaction and rendering a Transaction component for each */
+  }
   return (
-      <h3>History</h3> 
+    <>
+      <h3>History</h3>
       <ul className="list">
         {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
+    </>
   );
 };
 
 // return (
 //   <>
-//     <h3>History</h3> 
+//     <h3>History</h3>
 //     <ul className="list">
 //       {transactions.map((transaction) => (
 //         <Transaction key={transaction.id} transaction={transaction} />
@@ -29,4 +31,3 @@ export const TransactionList = () => {
 //   </>
 // );
 // };
-
