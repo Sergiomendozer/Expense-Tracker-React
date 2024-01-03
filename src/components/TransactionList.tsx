1a6f84e69444492bf24522ee9168f14233d0bebb
Sergio Mendoza <sergiomendozansx@gmail.com>
2024-01-03 20:47:28 +0000

    Update 2 files linked

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -19,15 +19,3 @@ export const TransactionList = () => {
     </>
   );
 };
-
-// return (
-//   <>
-//     <h3>History</h3>
-//     <ul className="list">
-//       {transactions.map((transaction) => (
-//         <Transaction key={transaction.id} transaction={transaction} />
-//       ))}
-//     </ul>
-//   </>
-// );
-// };
